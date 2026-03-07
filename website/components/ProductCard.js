716b01c39db3d8bfaae59abf8b23ea59b1b865e9
starrynighttech <starrynight8531@gmail.com>
2026-03-07 20:27:25 +0000

    push to git hub

---
+++ website/components/ProductCard.js
@@ -0,0 +1,13 @@
+export default function ProductCard(){
+  return(
+    <div style={{
+      border:"1px solid #ccc",
+      padding:"20px",
+      width:"200px"
+    }}>
+      <h3>Sample Product</h3>
+      <p>$10</p>
+      <button>Add to Cart</button>
+    </div>
+  )
+}
---
+++ website/components/WalletBalance.js
@@ -0,0 +1,7 @@
+export default function WalletBalance(){
+  return(
+    <div>
+      <h2>Balance: $0.00</h2>
+    </div>
+  )
+}
